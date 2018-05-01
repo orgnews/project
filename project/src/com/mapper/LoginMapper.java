b0fb8361a8713d2ff81eccbfdf0aa0d4650edc32
zm <725431792@qq.com>
2018-05-01 13:05:33 +0800

    黑名单、密码、登录完善

--- project/src/com/mapper/LoginMapper.java
+++ project/src/com/mapper/LoginMapper.java
@@ -7,4 +7,11 @@ import org.apache.ibatis.annotations.Param;
 public interface LoginMapper {
 
 	public List<Object> queryLogin(@Param("username")String username,@Param("password")String password);
+	public void addLogin(@Param("username")String username,@Param("password")String password);
+	public List<Object> queryLoginAll(@Param("startPage")int startPage,@Param("number")int number);
+	public int queryLoginCount();
+	
+	public void updateLogin(@Param("username")String username,@Param("password")String password);
+	
+	public void saveBlackStatus(@Param("username")String username,@Param("status")String status);
 }
